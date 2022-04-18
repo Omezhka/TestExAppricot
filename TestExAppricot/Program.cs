017c45aa1b0010677aea04a1929ec1e75d20b365
Наталья Аверкова <cynopterussphinx@gmail.com>
2022-04-18 14:13:29 +0300

    разделение методов обхода и подсчета размера папки

--- TestExAppricot/Program.cs
+++ TestExAppricot/Program.cs
@@ -1,22 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 
 class Program
 {
-    //static StringCollection log = new StringCollection();
     static void Main(string[] args)
     {
+        Console.Write("> ");
         //задаем папку для обхода
-        string rootDir = @"C:\Users\Наталья\source\repos\ConsoleApp3";
-       
-        //вызываем рекурсивный метод
-        Walk(new DirectoryInfo(rootDir));
+        string? rootDir = Console.ReadLine(); /*@"C:\Users\Наталья\source\repos\ConsoleApp3";*/
+        List<string> listWalk = new();
+        List<float> folderSize = new();
+
+       // folderSize = CalculateFolderSize(rootDir);
+        try
+        {
+            Walk(new DirectoryInfo(rootDir), listWalk);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            Console.WriteLine("Повторите ввод");
+            Main(args);
+        }
+        foreach (string dir in listWalk)
+        {
+            char[] charsToTrim = { '-', ' ' };
+            //folderSize.Add(CalculateFolderSize(dir.Trim(charsToTrim)));
+           // foreach (float size in folderSize)
+            Console.WriteLine(dir +" "+ CalculateFolderSize(dir.Trim(charsToTrim)) +  " main");
+        }
     }
-    static void Walk(DirectoryInfo root, string tire = "-")
+
+    static void Walk(DirectoryInfo root, List<string> listWalk, string tire = "-")
     {
-        FileInfo[] files = null;
-        DirectoryInfo[] subDirs = null;
-        double catalogSize = 0;
-      
+        
+        FileInfo[]? files = null;
+        DirectoryInfo[]? subDirs;
+        double catalogSize;
+
         // Получаем все файлы в текущем каталоге
         try
         {
@@ -28,30 +49,38 @@ class Program
         }
 
         if (files != null)
-        {   
+        {
             //получаем все подкаталоги
             subDirs = root.GetDirectories();
-            catalogSize += CalculateFolderSize(root.ToString());
-             Console.WriteLine($"{tire} {root.FullName} ({catalogSize} bytes) ");
-            //проходим по каждому подкаталогу
 
+            //получаем размер каталога
+          //  catalogSize = CalculateFolderSize(root.ToString());
+
+            //  Console.WriteLine($"{tire} {root.FullName} ({catalogSize} bytes) ");
+            listWalk.Add($"{tire} {root.FullName}");
             //выводим имена файлов в консоль
             foreach (FileInfo fi in files)
             {
-                Console.WriteLine($" {tire + "-"} {fi.Name} ({fi.Length} bytes)");
+                listWalk.Add($"{tire + "-"} {fi.Name} ({fi.Length} bytes)");
+               // Console.WriteLine($"{tire + "-"} {fi.Name} ({fi.Length} bytes)");
             }
 
+            //проходим по каждому подкаталогу
             foreach (DirectoryInfo dirInfo in subDirs)
-            {  
+            {
                 //РЕКУРСИЯ
-                Walk(dirInfo, tire + "-");
-            }   
+                Walk(dirInfo, listWalk, tire + "-");
+               
+            }
+           
         }
+        
     }
 
     protected static float CalculateFolderSize(string folder)
     {
         float folderSize = 0.0f;
+        
         try
         {
             //Checks if the path is valid or not
@@ -60,24 +89,25 @@ class Program
             else
             {
                 try
+            {
+                foreach (string file in Directory.GetFiles(folder))
                 {
-                    foreach (string file in Directory.GetFiles(folder))
+                    if (File.Exists(file))
                     {
-                        if (File.Exists(file))
-                        {
-                            FileInfo finfo = new FileInfo(file);
-                            folderSize += finfo.Length;
-                        }
+                        FileInfo finfo = new FileInfo(file);
+                        folderSize += finfo.Length;
+                        
                     }
+                }
 
-                    foreach (string dir in Directory.GetDirectories(folder))
-                        folderSize += CalculateFolderSize(dir);
-                }
-                catch (NotSupportedException e)
-                {
-                    Console.WriteLine("Unable to calculate folder size: {0}", e.Message);
-                }
+                foreach (string dir in Directory.GetDirectories(folder))
+                    folderSize += CalculateFolderSize(dir);
             }
+            catch (NotSupportedException e)
+            {
+                Console.WriteLine("Unable to calculate folder size: {0}", e.Message);
+            }
+             }
         }
         catch (UnauthorizedAccessException e)
         {
